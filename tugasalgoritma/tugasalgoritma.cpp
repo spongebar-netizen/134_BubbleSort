1af22a7e6610a599185650e97ad1e4b937298f4f
spongebar-netizen <fatanjrz1@gmail.com>
2024-03-08 11:18:59 +0700

    menambahkan kata"di"

--- tugasalgoritma/tugasalgoritma.cpp
+++ tugasalgoritma/tugasalgoritma.cpp
@@ -12,8 +12,8 @@ void input() {
 			break;
 		else {
 			cout << "\nArray dapat mempunyai maksimal 20 elemen.\n";
+		}
 	}
-}
 	cout << endl;
 	cout << "===========" << endl;
 	cout << "Masukkan Elemen Array" << endl;
@@ -21,3 +21,31 @@ void input() {
 
 	for (int i = 0; i < n; i++) {
 		cout << "Data ke berapa-" << (i + 1) << ": ";
+		cin >> a[i];
+	}
+}
+
+
+void bublesSortArray() {
+	int pass = 1;
+
+	do {
+		for (int j = 0; j <= n - 1 - pass; j++) {
+			if (a[j] > a[j + 1]) {
+				int temp;
+				temp = a[j];
+				a[j] = a[j + 1];
+				a[j + 1] = temp;
+			}
+		}
+		pass = pass + 1;
+	} while (pass <= n - 1);
+}
+
+void display() {
+	cout << endl;
+	cout << "===========" << endl;
+	cout << "element array yang telah disusun" << endl;
+	cout << "===========" << endl;
+	for (int j = 0; j < n ; j++) {
+}
