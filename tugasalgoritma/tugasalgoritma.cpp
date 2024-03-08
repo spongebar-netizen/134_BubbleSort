ecaf6ed2876c05a27b102af7c60a2c66fa66bf76
spongebar-netizen <fatanjrz1@gmail.com>
2024-03-08 10:55:45 +0700

    Add project files.

---
+++ tugasalgoritma/tugasalgoritma.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int a[20];
+int n;
+
+void input() {
+	while (true) {
+		cout << "masukkan banyaknya element array = ";
+		cin >> n;
+		if (n <= 20)
+			break;
+		else {
+			cout << "\nArray dapat mempunyai maksimal 20 elemen.\n";
+	}
+}
+	cout << endl;
+	cout << "==========" << endl;
+	cout << "Masukkan Elemen Array" << endl;
+	cout << "==========" << endl;
