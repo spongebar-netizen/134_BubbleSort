af620b162d05bcd37c542a4db34a33fd6e05d806
spongebar-netizen <fatanjrz1@gmail.com>
2024-03-08 13:07:16 +0700

    menambahkan kata"beberapa"

--- tugasalgoritma/tugasalgoritma.cpp
+++ tugasalgoritma/tugasalgoritma.cpp
@@ -16,7 +16,7 @@ void input() {
 	}
 	cout << endl;
 	cout << "===========" << endl;
-	cout << "Masukkan Elemen Array" << endl;
+	cout << "Masukkan beberapa Elemen Array" << endl;
 	cout << "===========" << endl;
 
 	for (int i = 0; i < n; i++) {
